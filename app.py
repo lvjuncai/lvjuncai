15fc0c2ba15df3902b5425bf264356396c07155d
lvjuncai <594695768@qq.com>
2018-10-16 19:10:06 +0800

    test

--- app.py
+++ app.py
@@ -1,27 +1,33 @@
 from flask import Flask
-from flask import Flask, render_template
+from flask import Flask, render_template,redirect,url_for
 from flask_bootstrap3 import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask import request
+from models import User
+from exts import db
+import pymysql
 import config
+pymysql.install_as_MySQLdb()
+
 
 app = Flask(__name__)
 db=SQLAlchemy(app)
 bootstrap=Bootstrap(app)
 app.config.from_object(config)
-
-"""class User(db.Model):
+db.init_app(app)
+"""
+class User(db.Model):
     __tablename__="user"
     id =db.Column(db.Integer,primary_key=True,autoincrement=True)
-    username=db.Column(db.String(100),nullable=False)
+    username=db.Column(db.String(50),nullable=False)
+    author_id =db.Column(db.Integer,db.ForeignKey("user.id"))
     password=db.Column(db.String(100),nullable=False)
 class Message(db.Model):
     __tablename__ = "Message"
     id = db.Column(db.Integer,primary_key=True,autoincrement=True)
     title= db.Column(db.String(100),nullable=False)
     content =db.Column(db.Text,nullable=False)
-    author_id =db.Column(db.Integer,db.ForeignKey("user.id"))
-
+   
 db.create_all()
 """
 @app.route('/')
@@ -39,8 +45,25 @@ def Login():
         return render_template("login.html")
     else:
         pass
-
-
+@app.route('/register/',methods=['GET','POST'])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        username=request.form.get("username")
+        password1=request.form.get("password1")
+        password2=request.form.get("password2")
+        own_user=User.query.filter(User.username == username).first()
+        if own_user:
+            return "该账户已被注册"
+        else:
+            if password1!=password2:
+                return '两次密码不一致！！'
+            else:
+                own_user =User(username=username,password=password1)
+                db.session.add(own_user)
+                db.session.commit()
+                return redirect(url_for("Login"))
 
 """def insert_db():
     article1=Message(title='aaa',content='bbb')
